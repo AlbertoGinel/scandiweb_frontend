b7070cfeec922b578d72b6874d8db46cb9e0fe6b
Alberto Ginel <aginelsalvador@gmail.com>
2024-07-18 16:26:29 +0300

    CORS

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -23,6 +23,7 @@ const ProductList = forwardRef(({ products, onChildUpdate }, ref) => {
         {
           method: "DELETE",
           body: JSON.stringify({ idList: checkedList }),
+          mode: "no-cors",
         }
       );
       if (response.ok) {
