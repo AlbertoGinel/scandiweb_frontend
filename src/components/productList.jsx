9fb34c3880b2133c42a67f9806c759bf4459bd25
Alberto Ginel <aginelsalvador@gmail.com>
2024-07-19 01:55:02 +0300

    CORS

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -21,6 +21,7 @@ const ProductList = forwardRef(({ products, onChildUpdate }, ref) => {
       const response = await fetch(
         "https://scandiweb-test-da56cd067ba1.herokuapp.com/products",
         {
+          mode: "no-cors",
           method: "DELETE",
           body: JSON.stringify({ idList: checkedList }),
           headers: { "Content-Type": "application/json" },
